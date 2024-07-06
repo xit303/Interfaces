a0fa0229b0c946f23797a2d40c77f1a60a622d18
xit303 <d.lange303@gmail.com>
2024-07-06 15:04:45 +0200

    style: :art: Prettier

--- ISettingsBase.h
+++ ISettingsBase.h
@@ -25,7 +25,6 @@ public:
     ISettingsBase(const char* name) : name(name) {}
 
     virtual bool HasChanged() = 0;
-
     virtual void SetDefault() = 0;
     virtual void Reload() = 0;
     virtual void Save() = 0;
