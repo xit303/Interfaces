5fda3642ecc8cf50a86c6167157df2e6b155eba0
xit303 <d.lange303@gmail.com>
2024-07-06 14:25:59 +0200

    feat: :tada: Initial commit

---
+++ Interfaces/IBaseClass.h
@@ -0,0 +1,12 @@
+#pragma once
+
+//******************************************************
+// Classes
+//******************************************************
+
+class IBaseClass
+{
+public:
+    virtual bool Init() = 0;
+    virtual void Update() = 0;
+};
---
+++ Interfaces/ISettingsBase.h
@@ -0,0 +1,32 @@
+#pragma once
+
+//******************************************************
+// Includes
+//******************************************************
+
+#include "IBaseClass.h"
+
+//******************************************************
+// Classes
+//******************************************************
+
+class ISettingsBase : public IBaseClass
+{
+protected:
+    const char* name;
+
+    virtual void OnInit() = 0;
+    virtual void OnSetDefault() = 0;
+    virtual void OnReload() = 0;
+    virtual void OnSave() = 0;
+    virtual void Print() {};
+
+public:
+    ISettingsBase(const char* name) : name(name) {}
+
+    virtual bool HasChanged() = 0;
+
+    virtual void SetDefault() = 0;
+    virtual void Reload() = 0;
+    virtual void Save() = 0;
+};
