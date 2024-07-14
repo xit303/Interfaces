937c6b26a3448edb30527ec10f599121da48fbeb
xit303 <d.lange303@gmail.com>
2024-07-14 19:13:55 +0200

    feat: :sparkles: Added forceSave to Save() method

--- ISettingsBase.h
+++ ISettingsBase.h
@@ -27,5 +27,5 @@ public:
     virtual bool HasChanged() = 0;
     virtual bool SetDefault() = 0;
     virtual bool Reload() = 0;
-    virtual bool Save() = 0;
+    virtual bool Save(bool forceSave = false) = 0;
 };
